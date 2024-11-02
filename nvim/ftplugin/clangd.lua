3b6ded06cdf94f33f8b3d926c29882e6e6a28e8c
Gopi Krishna Menon <krishnagopi487.github@outlook.com>
2024-11-02 22:34:48 +0530

    Bugfix

--- nvim/ftplugin/clangd.lua
+++ nvim/ftplugin/clangd.lua
@@ -5,11 +5,11 @@ end
 
 local root_files = {
   '.git',
-  '.compile_commands.json'
+  'compile_commands.json'
 }
 
 vim.lsp.start {
-  name = 'clangd',
+  name = 'clangd_lsp',
   cmd = { 'clangd' },
   root_dir = vim.fs.dirname(vim.fs.find(root_files, { upward = true })[1]),
   capabilities = require('user.lsp').make_client_capabilities(),
