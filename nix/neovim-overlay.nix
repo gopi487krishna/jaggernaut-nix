c640e1ef3114e8220b336625eb49796519a6c251
Gopi Krishna Menon <krishnagopi487.github@outlook.com>
2024-11-20 16:29:54 +0530

    Add render-markdown for better markdown experience

--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -88,6 +88,7 @@ with final.pkgs.lib; let
     # (mkNvimPlugin inputs.wf-nvim "wf.nvim") # (example) keymap hints | https://github.com/Cassin01/wf.nvim
     # ^ bleeding-edge plugins from flake inputs
     which-key-nvim
+    render-markdown
   ];
 
   extraPackages = with pkgs; [
