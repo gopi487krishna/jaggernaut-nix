ee49900a5750926ecf0e76f6d012e1590ef1a11d
Gopi Krishna Menon <krishnagopi487.github@outlook.com>
2024-11-20 16:47:52 +0530

    Fix typo

--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -88,7 +88,7 @@ with final.pkgs.lib; let
     # (mkNvimPlugin inputs.wf-nvim "wf.nvim") # (example) keymap hints | https://github.com/Cassin01/wf.nvim
     # ^ bleeding-edge plugins from flake inputs
     which-key-nvim
-    render-markdown
+    render-markdown-nvim
   ];
 
   extraPackages = with pkgs; [
