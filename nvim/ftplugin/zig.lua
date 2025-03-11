17f35d63ea5f637aa88e5fda5e8b1edbfdb9e3ce
Gopi Krishna Menon <krishnagopi487.github@outlook.com>
2025-03-11 08:38:47 +0530

    fix: Disable autoformat for zig code

--- nvim/ftplugin/zig.lua
+++ nvim/ftplugin/zig.lua
@@ -13,3 +13,6 @@ vim.lsp.start {
   root_dir = vim.fs.dirname(vim.fs.find(root_files, { upward = true })[1]),
   capabilities = require('user.lsp').make_client_capabilities(),
 }
+
+vim.g.zig_fmt_parse_errors = 0
+vim.g.zig_fmt_autosave = 0
