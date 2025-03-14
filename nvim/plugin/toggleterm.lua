97f1bc2ed85b262278f6f1caa01ef99b3718895b
Gopi Krishna Menon <krishnagopi487.github@outlook.com>
2025-03-14 12:13:11 +0530

    feat: Add some terminals to improve workflow
    fix: Minor Fixes
    
    fix: Adjust size to 80
    
    fix: Disable persist_size and instead follow size
    
    TMP
    
    TMP
    
    TMP
    
    TMP
    
    TMP\

--- nvim/plugin/toggleterm.lua
+++ nvim/plugin/toggleterm.lua
@@ -5,3 +5,55 @@ require("toggleterm").setup {
       border = 'curved'
     },
 }
+
+local Terminal  = require('toggleterm.terminal').Terminal
+
+-- Integrated Lazy Terminal
+local lazygit_term = Terminal:new({
+  cmd = "lazygit",
+  hidden = true,
+  direction = "float",
+  float_opts = {
+    border = "double",
+  },
+}
+)
+
+function LazygitTermToogle()
+  lazygit_term:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<leader>tg", "<cmd>lua LazygitTermToogle()<CR>", {noremap = true, silent = true})
+
+
+-- Integrated Aider Terminal
+local aider_term = Terminal:new({
+  cmd = "aider --model gemini/gemini-2.0-flash --watch-files",
+  hidden = true,
+  direction = "vertical",
+  on_open = function(self)
+    self:resize(vim.o.columns * 0.3)
+  end
+}
+)
+
+function AiderTermToggle()
+  aider_term:toggle()
+end
+vim.api.nvim_set_keymap("n", "<leader>ta", "<cmd>lua AiderTermToggle()<CR>", {noremap = true, silent = true})
+
+-- Simple Vertical Terminal
+local vertical_term = Terminal:new({
+  hidden = true,
+  direction = "vertical",
+  on_open = function(self)
+    self:resize(vim.o.columns * 0.3)
+  end
+}
+)
+
+function VerticalTermToggle()
+  vertical_term:toggle()
+end
+vim.api.nvim_set_keymap("n", "<leader>tv", "<cmd>lua VerticalTermToggle()<CR>", {noremap = true, silent = true})
+
