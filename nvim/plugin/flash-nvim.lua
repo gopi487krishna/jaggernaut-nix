9c55b64d121babd712937aa2f933b88a0cafa091
Gopi Krishna Menon <krishnagopi487.github@outlook.com>
2025-03-20 21:51:55 +0530

    chore(flash.nvim): Remove remote and treesitter_search keybinds

--- nvim/plugin/flash-nvim.lua
+++ nvim/plugin/flash-nvim.lua
@@ -6,6 +6,6 @@ local keymap = vim.keymap.set
 
 keymap({"n", "x", "o"}, "s", function() flash.jump() end, { desc = "Flash" })
 keymap({"n", "x", "o"}, "S", function() flash.treesitter() end, { desc = "Flash Treesitter" })
-keymap({"n", "x", "o"}, "r", function() flash.remote() end, { desc = "Remote Flash" })
-keymap({"n", "x", "o"}, "R", function() flash.treesitter_search() end, { desc = "Treesitter Search" })
+-- keymap({"n", "x", "o"}, "r", function() flash.remote() end, { desc = "Remote Flash" })
+-- keymap({"n", "x", "o"}, "R", function() flash.treesitter_search() end, { desc = "Treesitter Search" })
 keymap({"n", "x", "o"}, "<c-s>", function() flash.toggle() end, { desc = "Toggle Flash Search" })
