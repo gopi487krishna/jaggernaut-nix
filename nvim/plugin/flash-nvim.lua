73e5ba572eb0c2f9250fd70b234f043b43f06265
Gopi Krishna Menon <krishnagopi487.github@outlook.com>
2025-03-20 19:11:39 +0530

    chore(flash.nvim): Replace with default config

--- nvim/plugin/flash-nvim.lua
+++ nvim/plugin/flash-nvim.lua
@@ -4,29 +4,8 @@ flash.setup({});
 -- Keymappings
 local keymap = vim.keymap.set
 
--- Normal mode mappings
--- 's' for general flash search
-keymap("n", "s", function() flash.jump() end, { desc = "Flash" })
--- 'S' for treesitter search
-keymap("n", "S", function() flash.treesitter() end, { desc = "Flash Treesitter" })
--- Search with '/' and '?'
-keymap("n", "/", function() flash.jump({ pattern = vim.fn.input("Search: ") }) end, { desc = "Flash Search" })
-
--- Visual mode mappings
-keymap("x", "s", function() flash.jump() end, { desc = "Flash" })
-keymap("x", "S", function() flash.treesitter() end, { desc = "Flash Treesitter" })
-
--- Operator-pending mode mappings
-keymap("o", "s", function() flash.jump() end, { desc = "Flash" })
-keymap("o", "S", function() flash.treesitter() end, { desc = "Flash Treesitter" })
-
--- Additional useful mappings
--- Jump to word beginning
-keymap({"n", "x", "o"}, "f", function() flash.jump({ matcher = flash.matchers.char }) end, { desc = "Flash char" })
--- Jump to word ending
-keymap({"n", "x", "o"}, "F", function() flash.jump({ matcher = flash.matchers.char, reverse = true }) end, { desc = "Flash char backwards" })
--- Jump to word with 2-character search
-keymap({"n", "x", "o"}, "t", function() flash.jump({ matcher = flash.matchers.to }) end, { desc = "Flash to" })
--- Jump to word with 2-character search backwards
-keymap({"n", "x", "o"}, "T", function() flash.jump({ matcher = flash.matchers.to, reverse = true }) end, { desc = "Flash to backwards" })
-
+keymap({"n", "x", "o"}, "s", function() flash.jump() end, { desc = "Flash" })
+keymap({"n", "x", "o"}, "S", function() flash.treesitter() end, { desc = "Flash Treesitter" })
+keymap({"n", "x", "o"}, "r", function() flash.remote() end, { desc = "Remote Flash" })
+keymap({"n", "x", "o"}, "R", function() flash.treesitter_search() end, { desc = "Treesitter Search" })
+keymap({"n", "x", "o"}, "<c-s>", function() flash.toggle() end, { desc = "Toggle Flash Search" })
